d7ac1a667e2d7746cfef34eb9fc70855814030a2
Kenendy.Githua <githuanjuguna@gmail.com>
2026-07-14 17:03:24 +0300

    Added UserDTO and automapperprofiles

---
+++ RazorWebApps/FoRavers/FoRavers/Helpers/DTOs/AutoMapperProfiles.cs
@@ -0,0 +1,13 @@
+﻿using FoRavers.Models;
+using AutoMapper;
+
+namespace FoRavers.Helpers.DTOs
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<User, UserDTO>();
+        }
+    }
+}
---
+++ RazorWebApps/FoRavers/FoRavers/Helpers/DTOs/UserDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace FoRavers.Helpers.DTOs
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+
+        public string UserName {  get; set; }
+
+        public string Email { get; set; }
+
+        public string ProfilePhoto { get; set; }
+
+        public string MusicGenre { get; set; }
+    }
+}
