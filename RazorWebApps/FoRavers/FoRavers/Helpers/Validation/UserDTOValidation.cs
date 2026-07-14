8dcba89c2711d673a7cadff234d979edc99f2ca4
Kenendy.Githua <githuanjuguna@gmail.com>
2026-07-14 17:34:13 +0300

    Added UserDTO validation

---
+++ RazorWebApps/FoRavers/FoRavers/Helpers/Validation/UserDTOValidation.cs
@@ -0,0 +1,18 @@
+﻿using FoRavers.Helpers.DTOs;
+using FluentValidation;
+
+namespace FoRavers.Helpers.Validation
+{
+    public class UserDTOValidation : AbstractValidator<UserDTO>
+    {
+        public UserDTOValidation() 
+        {
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Email is required")
+                .EmailAddress().WithMessage("Invalid email format");
+            RuleFor(x => x.MusicGenre).NotEmpty().WithMessage("Music genre is required");
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("Username is required")
+                .Matches(@"\A\S{3,15}\z").WithMessage("Username must be at least 3 characters long");
+            RuleFor(x => x.ProfilePhoto).NotEmpty().WithMessage("Profile photo is required");
+        }
+    }
+}
