ba6421fb3b3c7dcdb39bd0d097f0b6ceb829fa60
Kenendy.Githua <githuanjuguna@gmail.com>
2026-07-15 20:47:52 +0300

    Add FollowRequest and FollowTargetSummary DTO classes
    Introduced FollowRequest and FollowTargetSummary classes in the FoRavers.Helpers.DTOs namespace to represent follow request data and target summaries, respectively.

---
+++ RazorWebApps/FoRavers/FoRavers/Helpers/DTOs/FollowRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace FoRavers.Helpers.DTOs
+{
+    public class FollowRequest
+    {
+        public string TargetId { get; set; } = string.Empty;
+
+        public string Target { get; set; } = string.Empty;
+    }
+}
