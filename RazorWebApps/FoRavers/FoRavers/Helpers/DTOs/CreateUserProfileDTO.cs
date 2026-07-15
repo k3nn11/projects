abf0baea45660b4f879177dc5411e508b8ff4fb0
Kenendy.Githua <githuanjuguna@gmail.com>
2026-07-15 14:34:17 +0300

    Add CreateUserProfileDTO and validation class
    Introduced CreateUserProfileDTO for user profile creation and CreateUserProfileDTOValidation for input validation. Removed old UserDTOValidation and references to UpdateUserProfileDTO. Updated .gitignore to track new validation file.

---
+++ RazorWebApps/FoRavers/FoRavers/Helpers/DTOs/CreateUserProfileDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace FoRavers.Helpers.DTOs
+{
+    public class CreateUserProfileDTO
+    {
+        public string UserName { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public string ProfilePhoto { get; set; } = null!;
+
+        public string? MusicGenre { get; set; }
+    }
+}
--- RazorWebApps/FoRavers/FoRavers/Helpers/Validation/CreateUserProfileDTOValidation.cs
+++ RazorWebApps/FoRavers/FoRavers/Helpers/Validation/CreateUserProfileDTOValidation.cs
@@ -3,13 +3,12 @@ using FluentValidation;
 
 namespace FoRavers.Helpers.Validation
 {
-    public class UserDTOValidation : AbstractValidator<UpdateUserProfileDTO>
+    public class CreateUserProfileDTOValidation : AbstractValidator<CreateUserProfileDTO>
     {
-        public UserDTOValidation() 
+        public CreateUserProfileDTOValidation() 
         {
             RuleFor(x => x.Email).NotEmpty().WithMessage("Email is required")
                 .EmailAddress().WithMessage("Invalid email format");
-            RuleFor(x => x.MusicGenre).NotEmpty().WithMessage("Music genre is required");
             RuleFor(x => x.UserName).NotEmpty().WithMessage("Username is required")
                 .Matches(@"\A\S{3,15}\z").WithMessage("Username must be at least 3 characters long");
             RuleFor(x => x.ProfilePhoto).NotEmpty().WithMessage("Profile photo is required");
