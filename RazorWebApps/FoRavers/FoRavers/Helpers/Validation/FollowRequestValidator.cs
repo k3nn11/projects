51154aae027fce180cd85f2ba6547dc642370e33
Kenendy.Githua <githuanjuguna@gmail.com>
2026-07-15 20:57:08 +0300

    Add FollowRequestValidator for request validation
    Introduced FollowRequestValidator  to ensure TargetId is not empty and Target is a valid enum value ('Promoter' or 'Venue'), with custom error messaging for invalid targets.

---
+++ RazorWebApps/FoRavers/FoRavers/Helpers/Validation/FollowRequestValidator.cs
@@ -0,0 +1,17 @@
+﻿using FoRavers.Models;
+using FoRavers.Helpers.DTOs;
+using FluentValidation;
+
+namespace FoRavers.Helpers.Validation
+{
+    public class FollowRequestValidator : AbstractValidator<FollowRequest>
+    {
+       public FollowRequestValidator()
+        {
+            RuleFor(x => x.TargetId).NotEmpty();
+            RuleFor(x => x.Target).NotEmpty()
+                .Must(t => Enum.TryParse<Target>(t, ignoreCase: true, out _))
+                .WithMessage("Invalid target value. Must be either 'Promoter' or 'Venue'.");
+        }
+    }
+}
