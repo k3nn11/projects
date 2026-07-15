213175da0ca8ddef9cb9cb06a6e24f865f4f2400
Kenendy.Githua <githuanjuguna@gmail.com>
2026-07-15 14:51:07 +0300

    Update AssemblyInformationalVersion in AssemblyInfo.cs
    Updated AssemblyInformationalVersionAttribute to "1.0.0+2cc61306862712346a48df662747e592c878c19f" to reflect the latest build metadata. No other changes were made.

--- RazorWebApps/FoRavers/FoRavers/obj/Debug/net10.0/FoRavers.AssemblyInfo.cs
+++ RazorWebApps/FoRavers/FoRavers/obj/Debug/net10.0/FoRavers.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoRavers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d004f687785fb10ce642040f28aa3ff574e7d0df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cc61306862712346a48df662747e592c878c19f")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoRavers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoRavers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
