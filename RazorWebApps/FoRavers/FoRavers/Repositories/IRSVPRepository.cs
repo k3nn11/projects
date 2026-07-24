26dbd159146b2ab6251e880cf832d6a7305f1288
Kenendy.Githua <githuanjuguna@gmail.com>
2026-07-24 17:28:51 +0300

    Add IRSVPRepository interface and RSVPRepository implementation
    Introduced IRSVPRepository with GetUserAndEventAsync method for fetching RSVP by user and event IDs. Implemented this interface in RSVPRepository using Entity Framework Core.

---
+++ RazorWebApps/FoRavers/FoRavers/Repositories/IRSVPRepository.cs
@@ -0,0 +1,9 @@
+﻿using FoRavers.Models;
+
+namespace FoRavers.Repositories
+{
+    public interface IRSVPRepository : IRepository<RSVP>
+    {
+        public Task<RSVP?> GetUserAndEventAsync(Guid userId, Guid eventId);
+    }
+}
---
+++ RazorWebApps/FoRavers/FoRavers/Repositories/RSVPRepository.cs
@@ -0,0 +1,22 @@
+﻿using FoRavers.Models;
+using FoRavers.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoRavers.Repositories
+{
+    public class RSVPRepository : Repository<RSVP>, IRSVPRepository
+    {
+
+        public RSVPRepository(FoRaversDbContext context) : base(context)
+        {
+        }
+
+        public async Task<RSVP?> GetUserAndEventAsync(Guid userId, Guid eventId)
+        {
+            return await _context.RSVPs
+                .Where(rsvp => rsvp.UserId == userId && rsvp.EventId == eventId)
+                .FirstOrDefaultAsync();
+
+        }
+    }
+}
